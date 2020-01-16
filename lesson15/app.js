bdddb4eb928e2722dd404b7c3291540726f1acbc
= <ianprice943@yahoo.com>
2020-01-15 21:52:17 -0500

    Lesson 15 complete

---
+++ lesson15/app.js
@@ -0,0 +1,23 @@
+Vue.component('greeting', {
+    template: '<p>Hey there, I am {{ name }}. <button v-on:click="changeName">Change name</button></p>',
+    data: function() {
+        return {
+            name: 'Yoshi'
+        }
+    },
+    methods: {
+        changeName: function() {
+            this.name = 'Mario';
+        }
+    }
+});
+
+var one = new Vue({
+    el: '#vue-app-one',
+    
+});
+
+var two = new Vue({
+    el: '#vue-app-two',
+    
+});

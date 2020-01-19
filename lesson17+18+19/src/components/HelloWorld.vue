c789a3fa379b5f80f28be928fb92edeb9f1984a9
= <ianprice943@yahoo.com>
2020-01-19 18:44:22 -0500

    Lesson 19 complete

---
+++ lesson17+18+19/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+import Ninjas from './Ninjas.vue'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    'ninjas': Ninjas
+  },
+  data() {
+    return {
+      title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ lesson17+18+19/src/components/Ninjas.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="(ninja, index) in ninjas" v-bind:key="index">{{ ninja }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            ninjas: ['Yoshi', 'Mario', 'Ryu']
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
